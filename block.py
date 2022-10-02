7fe92997c7435c69cfd3f6956d6696bcd70d0e24
jeanmariemcc <jeanmarie@mccormackmomentum.com>
2022-10-02 14:45:23 -0400

    add timestamp and hashes to Block

--- block.py
+++ block.py
@@ -3,11 +3,20 @@ class Block:
     Unit of storage for the Blockchain
       stores transactions and supports a cryptocurrency
     """
-    def __init__ (self, data):
+    def __init__ (self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
         self.data = data
         
     def __repr__(self):
-        return f'Block - data: {self.data}'
+        return (
+            'Block('
+            f'timestamp: {self.timestamp},'
+            f'last_hash: {self.last_hash},'
+            f'hash: {self.hash}'
+            f'data: {self.data}'
+            )
         
 def main():
     block = Block('test_foo')
