fdffeba0845d5c3da8c1983e328ca6bf9ce8cd12
jeanmariemcc <jeanmarie@mccormackmomentum.com>
2022-10-04 11:48:47 -0400

    Validate the block

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -63,6 +63,37 @@ class Block:
     def genesis():
         # generate the first block
         return Block(**GENESIS_DATA)
+    
+    
+    @staticmethod
+    def is_valid_block(last_block, block):
+        """
+        Validate block by enforcing the following rules
+        -block must have the proper last_hash reference
+        -block must meet the proof of work requment (number of 0s)
+        -block hash must be a valid combo of the block fields
+        """
+        if block.last_hash != last_block.hash:
+            raise Exception('The block last_hash must be correct')
+        
+        if block.hash[0:block.difficulty] != '0' * block.difficulty:
+            raise Exception('The proof of Work requirement was not met')
+        
+        if (abs(last_block.difficulty - block.difficulty) > 1):
+            raise Exception('The block difficulty can only be changed by 1')
+        
+        reconstructed_hash = crypto_hash(
+            block.timestamp,
+            block.last_hash,
+            block.data,
+            block.nonce,
+            block.difficulty
+        )
+        
+        if block.hash != reconstructed_hash:
+            raise Exception('The block hash does not match, it is not correct')
+        
+            
 
         
 def main():
@@ -70,9 +101,19 @@ def main():
     #print(block)
     #print(f'block.py __name__: {__name__}')
     
+    #genesis_block = Block.genesis()
+    #block = Block.mine_block(genesis_block, 'firstblock - genesis')
+    #print(block)
+
+    #test block validation
     genesis_block = Block.genesis()
-    block = Block.mine_block(genesis_block, 'firstblock - genesis')
-    print(block)
+    bad_block = Block.mine_block(Block.genesis(), 'foo')
+    #comment the following line out to test a good block
+    bad_block.last_hash = 'evil_data'
+    try:
+        Block.is_valid_block(genesis_block, bad_block)
+    except Exception as e:
+        print(f'is_valid_block: {e}')
     
 if __name__ == '__main__':
     # only prints if called directly, not if called from blockchain.py
